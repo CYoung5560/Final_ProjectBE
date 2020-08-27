04965441ee36edc44f134ecf11a5d5b22f8b3d41
Connor <Connor.young@qa.com>
2020-08-27 16:18:33 +0100

    update to schema

--- models/ticket.model.js
+++ models/ticket.model.js
@@ -6,16 +6,15 @@ const ticketSchema = new mongoose.Schema({
 	screening: {type: mongoose.Types.ObjectId, ref: 'times'}; 
 	booker: {type: mongoose.Types.ObjectId, ref: 'user'};
 	concession: {type: mongoose.Types.ObjectId, ref: 'concession'};
-    
 
 })
 
 ticketSchema.methods.info = () => {
-    console.log(`Title -> this.movieTitle`);
-	console.log(`Screening -> this.screening`);
-	console.log(`Booker -> this.booker`);
-	console.log(`Concession -> this.concession`);
-	console.log(`MIN -> this.minute`); 
+    console.log(`TITLE -> this.movieTitle`);
+	console.log(`SCREENING -> this.screening`);
+	console.log(`BOOKER -> this.booker`);
+	console.log(`CONCESSION -> this.concession`);
+	console.log(`MINUTE -> this.minute`); 
 }
 
 const tickets = mongoose.model('times', schema); 
