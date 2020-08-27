34387326d36bae64455d0bae4e0da86dcb3cbecb
MrWalshyType2 <mwalsh@academytrainee.com>
2020-08-27 16:35:11 +0100

    Fixed conflict

--- models/user.model.js
+++ models/user.model.js
@@ -10,11 +10,7 @@ const userSchema = new mongoose.Schema({
   role: String
 });
 
-<<<<<<< HEAD
-const userSchema = new mongoose.Schema({
-=======
 userSchema.plugin(passportLocalMongoose);
->>>>>>> 8e1e7f4450e2c72a98f5d9021d7ec63b260cb5f6
 
 const User = mongoose.model('User', userSchema, 'User');
 
