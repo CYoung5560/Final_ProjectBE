546afdbbaa84753c83fac4268036d76b3da5ff5e
MrWalshyType2 <mwalsh@academytrainee.com>
2020-08-27 16:27:29 +0100

    Fixed year type in movie schema, changed to Number

--- models/movie.model.js
+++ models/movie.model.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const movieSchema = new mongoose.Schema({
 
     title: {type: String, required: true},
-    year: {type: int, required: true},
+    year: {type: Number, required: true},
     description: {type: String},
     actors: {type: String},
     director: {type: String, required: true},
@@ -24,7 +24,7 @@ movieSchema.methods.info = () => {
 }
 
 // Compiled schema into a model.
-const Movie = mongoose.model('Movie', schema);
+const Movie = mongoose.model('Movie', movieSchema);
 module.export = Movie;
 
 // SEE FOR MORE ON MONGOOSE 5.10: https://mongoosejs.com/docs/
