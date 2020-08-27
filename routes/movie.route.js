23fb6ef06da209067704b87efc892f4ef71aa262
MrWalshyType2 <mwalsh@academytrainee.com>
2020-08-27 19:28:10 +0100

    Fixed user login

--- routes/movie.route.js
+++ routes/movie.route.js
@@ -16,18 +16,18 @@ router.use((request, response, next) => {
 });
 
 // Middleware to validate a route
-router.param('/:id', (request, response, next) => {
-    // do some validation.
-    console.log(`Validating ${request.url}`);
-    // Once validation is done, save new item in request.
-    // go to next thing (/:id route).
-    next();
-});
+// router.param('/:id', (request, response, next) => {
+//     // do some validation.
+//     console.log(`Validating ${request.url}`);
+//     // Once validation is done, save new item in request.
+//     // go to next thing (/:id route).
+//     next();
+// });
 
 // The routes will need to be modified to reflect using the request body at some point.
 // Pass control (via callback) to MovieController.
-router.get('/:id', passport.authenticate('jwt', { session: false }), checkIsInRole(ROLES.Customer), MovieController.getMovieById);
+router.get('/:id', MovieController.getMovieById);
 
-router.post('/:title', MovieController.createMovie);
+router.post('/', passport.authenticate('jwt', { session: false }), checkIsInRole(ROLES.Admin), MovieController.createMovie);
 
 module.exports = router;
