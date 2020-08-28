cf9296d5d94a91ac4c4da475ce8fd3d4018c87e4
MrWalshyType2 <mwalsh@academytrainee.com>
2020-08-28 09:57:32 +0100

    Added controller methods for ticket routes

--- controllers/ticket.controller.js
+++ controllers/ticket.controller.js
@@ -1,1 +1,41 @@
 const TicketService = require('../services/ticket.service');
+
+exports.getTicket = async (request, response, next) => {
+
+    try {
+        const ticket = await TicketService.getTicket(request.body.id);
+        return response.status(200).json({ status: 200, data: ticket, message: "ticket.controller -> Successfully got ticket from db" });
+    } catch(error) {
+        return response.status(400).json({ status: 400, message: error.message });
+    }
+};
+
+exports.createTicket = async (request, response, next) => {
+
+    try {
+        const ticket = await TicketService.createTicket(request.body);
+        return response.status(200).json({ status: 200, data: ticket, message: "ticket.controller -> Successfully created ticket in db" });
+    } catch(error) {
+        return response.status(400).json({ status: 400, message: error.message });
+    }
+};
+
+exports.updateTicket = async (request, response, next) => {
+
+    try {
+        const ticket = await TicketService.updateTicket(request.body);
+        return response.status(200).json({ status: 200, data: ticket, message: "ticket.controller -> Successfully updated ticket in db" });
+    } catch(error) {
+        return response.status(400).json({ status: 400, message: error.message });
+    }
+}
+
+exports.deleteTicket = async (request, response, next) => {
+
+    try {
+        await TicketService.deleteTicket(request.body.id);
+        return response.status(200).json({ status: 200, message: "ticket.controller -> Successfully deleted ticket in db" });
+    } catch(error) {
+        return response.status(400).json({ status: 400, message: error.message });
+    }
+}
